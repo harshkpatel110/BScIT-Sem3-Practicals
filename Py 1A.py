381c9ceff8abeb4d37f2f00c0f9db317be1bede2
harshkpatel110 <harsh.patelm.99@gmail.com>
2026-09-13 16:28:11 +0530

    Import datetime in Python 1A Practical

--- Py 1A.py
+++ Py 1A.py
@@ -1,5 +1,7 @@
 name = input("Enter your name: ")
 age = int(input("Enter your age in years: "))
+import datetime
+year = datetime.datetime.now().year
 if age<100:
     print(f"Hello {name}. You will turn 100 years old in the year {2026-age+100}.")
 elif age==100:
