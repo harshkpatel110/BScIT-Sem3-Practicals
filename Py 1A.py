35edfd47bd5c4a1eb19783978b2198de5e0ee035
harshkpatel110 <harsh.patelm.99@gmail.com>
2026-09-13 16:28:11 +0530

    Python 1A Practical complete

--- Py 1A.py
+++ Py 1A.py
@@ -3,8 +3,8 @@ age = int(input("Enter your age in years: "))
 import datetime
 year = datetime.datetime.now().year
 if age<100:
-    print(f"Hello {name}. You will turn 100 years old in the year {2026-age+100}.")
+    print(f"Hello {name}. You will turn 100 years old in the year {year - age + 100}.")
 elif age==100:
     print(f"Hello {name}, you are 100 years old.")
 elif age>100:
-    print(f"Hello {name}, you already turned 100 years old {age-100} years ago.")
+    print(f"Hello {name}, you already turned 100 years old in the year {year - age + 100}.")
