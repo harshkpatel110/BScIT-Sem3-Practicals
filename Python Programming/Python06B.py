0ae673bcf8565fcbc96505b432217c736ad9ce50
harshkpatel110 <harsh.patelm.99@gmail.com>
2026-09-22 18:46:25 +0530

    Python rename

--- Python Programming/Python06B.py
+++ Python Programming/Python06B.py
@@ -1,10 +1,10 @@
-import Python6Bgeometry
+import Python06Bgeometry
 choice=int(input("1. Square\n2. Circle\nEnter your choice: "))
 if choice==1:
     side=float(input("Enter side: "))
-    print("Area =",Python6Bgeometry.squareArea(side))
+    print("Area =",Python06Bgeometry.squareArea(side))
 elif choice==2:
     radius=float(input("Enter radius: "))
-    print("Area =",Python6Bgeometry.circleArea(radius))
+    print("Area =",Python06Bgeometry.circleArea(radius))
 else:
     print("Invalid Choice")
